ffda62aa144e592ce27e461532c0cad3a19f57cc
David W. Roberts <david.w.roberts91@gmail.com>
2020-04-18 17:52:58 +0100

    Super simple string concat implemented as C call.

--- em_c_bindings.c
+++ em_c_bindings.c
@@ -4,6 +4,7 @@
 #include "eso_debug.h"
 
 #include <stdio.h>
+#include <string.h>
 
 void stdio_prints(em_state* state) {
     em_stack_item* str = stack_top(state);
@@ -41,8 +42,40 @@ void stdio_print_bytes(em_state* state) {
     printf("\n");
 }
 
+void string_cat(em_state* state) {
+
+     em_stack_item* a = stack_top_minus(state, 1);
+     em_stack_item* b = stack_top(state);
+
+     if (a == NULL || a->code != 's' || b == NULL || b->code != 's') {
+        em_panic(state, "Expected two strings (s) to concatenate");
+     }
+
+    int a_nonul_size = (a->u.v_mptr->size - 1);
+    int b_nonul_size = (b->u.v_mptr->size - 1);
+
+    em_managed_ptr* mptr = create_managed_ptr(state);
+
+    mptr->size =  a_nonul_size + b_nonul_size + 1; 
+    mptr->raw = em_usercode_alloc(state, mptr->size, false);
+    mptr->concrete_type = NULL;
+    mptr->references++; // Stack holds a reference
+    memset(mptr->raw, 0, mptr->size);
+
+    memcpy(mptr->raw, a->u.v_mptr->raw, a_nonul_size);
+    memcpy(mptr->raw + a_nonul_size, b->u.v_mptr->raw, b_nonul_size);
+
+    stack_pop(state);
+    stack_pop(state);
+
+    int top = stack_push(state);
+    state->stack[top].code = 's';
+    state->stack[top].u.v_mptr = mptr; 
+}
+
 void em_bind_c_default(em_state* state) {
     em_bind_c_call(state, "stdio.prints", stdio_prints);
     em_bind_c_call(state, "stdio.print_bytes", stdio_print_bytes);
     em_bind_c_call(state, "debug.assert_no_leak", debug_leakcheck);
+    em_bind_c_call(state, "string.cat", string_cat);
 }
